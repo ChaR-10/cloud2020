d3ffaf93f58a4afb41a691161d0e09d483ccbdc6
ChaR <1215585066@qq.com>
2021-09-30 18:20:39 +0800

    feat:自定义负载均衡算法 实现轮询

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/lb/LoadBalancer.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.lb;
+
+import org.springframework.cloud.client.ServiceInstance;
+
+import java.util.List;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/9/30 18:12
+ */
+public interface LoadBalancer {
+    //收集服务器总共有多少台能够提供服务的机器，并放到list里面
+    ServiceInstance instances(List<ServiceInstance> serviceInstances);
+
+}
+
