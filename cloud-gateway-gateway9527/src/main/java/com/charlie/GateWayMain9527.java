8a90ad6fbb8e5024315ca230e5c6c2fb8affa14f
ChaR <1215585066@qq.com>
2021-10-01 22:43:18 +0800

    feat:Gateway, 网关通过yml进行路由配置访问支付服务8001

---
+++ cloud-gateway-gateway9527/src/main/java/com/charlie/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.charlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/1 20:47
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run( GateWayMain9527.class,args);
+    }
+}
