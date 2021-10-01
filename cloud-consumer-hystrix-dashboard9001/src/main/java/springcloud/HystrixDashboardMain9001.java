02748931893119eda704b4eec9318c4c127424f0
ChaR <1215585066@qq.com>
2021-10-01 20:32:43 +0800

    feat:Hystrix 服务监控hystrixDashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/1 17:51
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
+
