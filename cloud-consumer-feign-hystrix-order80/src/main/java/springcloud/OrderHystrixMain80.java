2d67b195c18ec8c664075427db343de1a26c34b0
ChaR <1215585066@qq.com>
2021-10-01 11:35:59 +0800

    feat:Hystrix 添加消费端，正常测试;配合Jmeter压力测试

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,19 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/1 11:15
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
+
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/springcloud/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,36 @@
+package springcloud.springcloud.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import springcloud.springcloud.service.PaymentHystrixService;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/1 11:18
+ */
+@RestController
+@Slf4j
+public class OrderHystrixController {
+
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_Timeout(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        return result;
+    }
+
+}
