cf7ea946f42e5ce305a2dc5d351f3d0f87a3ec63
ChaR <1215585066@qq.com>
2021-10-01 22:46:56 +0800

    feat:Gateway, 网关通过代码注入RouteLocator的Bean配置实现路由映射

---
+++ cloud-gateway-gateway9527/src/main/java/com/charlie/config/GateWayConfig.java
@@ -0,0 +1,22 @@
+package com.charlie.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/1 22:44
+ */
+
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_rote_atguigu", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}
