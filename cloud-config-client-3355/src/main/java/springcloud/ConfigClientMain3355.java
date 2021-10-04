35667fab5954013044a65fbe891dd8242002833c
ChaR <1215585066@qq.com>
2021-10-04 16:44:36 +0800

    feat:Config客户端配置与测试

---
+++ cloud-config-client-3355/src/main/java/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,16 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/4 16:37
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3355.class,args);
+    }
+}
+
