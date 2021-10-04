455b269b8aa786c73da33ba89fec4754a367f8a4
ChaR <1215585066@qq.com>
2021-10-04 16:25:00 +0800

    feat:Config服务端配置与测试

---
+++ cloud-config-center-3344/src/main/java/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/4 15:42
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344 .class,args);
+    }
+}
