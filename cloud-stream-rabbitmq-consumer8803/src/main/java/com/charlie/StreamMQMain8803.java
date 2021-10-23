d18a33661c114672292f036af812f27c511385b1
ChaR <1215585066@qq.com>
2021-10-23 18:07:57 +0800

    feat:SpringCloud stream :增加消费者8803

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/charlie/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.charlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/23 17:57
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/charlie/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,28 @@
+package com.charlie.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: ChaR
+ * @Date: 2021/10/23 17:57
+ */
+@Component
+@Slf4j
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println("消费者1号，------>接收到的消息："+message.getPayload()+"\t port" +serverPort);
+    }
+
+}
